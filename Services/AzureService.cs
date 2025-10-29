3056cefb33acc4a314da19f19ff415b43e87a8c5
“Jan <j.Votocek@commend.site>
2025-10-29 22:03:19 +0100

    update .net and deps

--- Services/AzureService.cs
+++ Services/AzureService.cs
@@ -1,29 +1,24 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace pokuk_common.services
 {
     internal class AzureService
     {
-        private GalleryOptions _config;
+        private readonly GalleryOptions _config;
+        private readonly BlobServiceClient _blobServiceClient;
+        private BlobContainerClient _container;
 
-        private CloudStorageAccount _azureAccount;
-        private CloudBlobContainer _container;
-
-        private CloudBlobContainer BlobContainer
+        private BlobContainerClient BlobContainer
         {
             get
             {
                 if (_container == null)
                 {
-                    var serviceClient = _azureAccount.CreateCloudBlobClient();
-                    _container = serviceClient.GetContainerReference(_config.AzureContainerName);
+                    _container = _blobServiceClient.GetBlobContainerClient(_config.AzureContainerName);
                 }
                 return _container;
             }
@@ -32,7 +27,7 @@ namespace pokuk_common.services
         public AzureService(GalleryOptions config)
         {
             _config = config;
-            _azureAccount = CloudStorageAccount.Parse(_config.AzureStorageConnectionString);
+            _blobServiceClient = new BlobServiceClient(_config.AzureStorageConnectionString);
         }
 
         /// Uploads gallery event images file to azure
@@ -47,52 +42,36 @@ namespace pokuk_common.services
         /// Uploads one gallery image file to azure
         private void Upload(IGalleryFile galleryFile)
         {
-            CloudBlockBlob blob = BlobContainer.GetBlockBlobReference(galleryFile.AzureLikeFileName);
-            blob.Properties.ContentType = "image/jpeg";
+            var blobClient = BlobContainer.GetBlobClient(galleryFile.AzureLikeFileName);
+            var headers = new BlobHttpHeaders { ContentType = "image/jpeg" };
 
-            System.Console.WriteLine($"Uploading to azure with name: {blob.Name}");
+            Console.WriteLine($"Uploading to azure with name: {blobClient.Name}");
 
             using (FileStream fs = File.Open(galleryFile.FullFileName, FileMode.Open))
             {
-                blob.UploadFromStreamAsync(fs).Wait();
+                blobClient.Upload(fs, new BlobUploadOptions { HttpHeaders = headers });
             }
         }
 
         public void UploadJson(IGallery gallery)
         {
-            CloudBlockBlob blob = BlobContainer.GetBlockBlobReference(_config.GalleryJsonName);
-            blob.Properties.ContentType = "application/json";
+            var blobClient = BlobContainer.GetBlobClient(_config.GalleryJsonName);
+            var headers = new BlobHttpHeaders { ContentType = "application/json" };
 
-            System.Console.WriteLine($"Uploading gallery json to azure with name: {blob.Name}");
+            Console.WriteLine($"Uploading gallery json to azure with name: {blobClient.Name}");
 
-            using (var ms = new MemoryStream())
-            {
-                var json = JsonConvert.SerializeObject(gallery);
-                using (var writer = new StreamWriter(ms))
-                {
-                    writer.Write(json);
-                    writer.Flush();
-                    ms.Position = 0;
-
-                    blob.UploadFromStreamAsync(ms).Wait();
-                }
-            }
+            var json = JsonConvert.SerializeObject(gallery);
+            using var ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json));
+            blobClient.Upload(ms, new BlobUploadOptions { HttpHeaders = headers });
         }
 
         /// Loads gallery json from azure
         public IGallery ReadGallery()
         {
-            Gallery model = null;
-            CloudBlockBlob blob = BlobContainer.GetBlockBlobReference(_config.GalleryJsonName);
-
-            string text;
-            using (var memoryStream = new MemoryStream())
-            {
-                blob.DownloadToStreamAsync(memoryStream).Wait();
-                text = System.Text.Encoding.UTF8.GetString(memoryStream.ToArray());
-            }
-
-            model = JsonConvert.DeserializeObject<Gallery>(text);
+            var blobClient = BlobContainer.GetBlobClient(_config.GalleryJsonName);
+            var download = blobClient.DownloadContent();
+            var text = download.Value.Content.ToString();
+            var model = JsonConvert.DeserializeObject<Gallery>(text);
             return model;
         }
 
